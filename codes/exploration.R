e0b4d84075c7de944fe1553cd55d2a3f08860815
Rajat Shah <rajat.shah200792@gmail.com>
2016-03-30 17:08:23 -0400

    Update exploration.R

--- codes/exploration.R
+++ codes/exploration.R
@@ -1,7 +1,12 @@
 # View any training instance as an image
 train <- read.csv("../input/train.csv", stringsAsFactors=FALSE) 
-rows = 28
-cols = 28
-rowID = 2
-m = matrix(as.integer(train[rowID,]), nrow = rows, ncols = cols)  #Convert row to a pixel matrix
-image(m)  #Plots the matrix
+rows <- 28
+cols <- 28
+rowID <- 2
+m <- matrix(as.integer(train[rowID,]), nrow = rows, ncol = cols)  #Convert row to a pixel matrix
+image(m)  #Plots the pixel matrix
+
+#Gray scale image plot
+#Color ramp def. 
+cus_col <- colorRampPalette(colors = c('white','black'))
+image(m, col = cus_col(256))
