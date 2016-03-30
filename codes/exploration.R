443bdafc0756aa9772ec5cdc4713de8c0eb79c08
Rajat Shah <rajat.shah200792@gmail.com>
2016-03-30 16:56:02 -0400

    Update exploration.R

--- codes/exploration.R
+++ codes/exploration.R
@@ -3,5 +3,5 @@ train <- read.csv("../input/train.csv", stringsAsFactors=FALSE)
 rows = 28
 cols = 28
 rowID = 2
-m = matrix(as.integer(train[rowID,]), nrow = rows, ncols = cols)  #Concert row to a pixel matrix
+m = matrix(as.integer(train[rowID,]), nrow = rows, ncols = cols)  #Convert row to a pixel matrix
 image(m)  #Plots the matrix
